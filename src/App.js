b7ef45ca166e2f59546c4f1f0f8cdcb9f7b97792
alfirrizqi <ivan_alfirrizqi@outlook.com>
2023-04-08 12:24:47 +0700

    testin01

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from 'react'
-import Navbar from './components/Navbar'
+import React from 'react';
+import Navbar from './components/Navbar';
+import MidText from './components/MidText'
 
 function App() {
   return (
     <div>
    <Navbar />
+   <MidText />
     </div>
   );
 }
---
+++ src/components/MidText.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const MidText = () => {
+    return (
+        <div className='text-white'>
+            <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
+                
+            </div>
+        </div>
+    )
+}
+
+export default MidText
