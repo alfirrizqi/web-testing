bc36201f66fc9c2f3e4a46ae6cb144807801e73a
alfirrizqi <ivan_alfirrizqi@outlook.com>
2023-04-08 12:44:15 +0700

    s

--- src/components/MidText.jsx
+++ src/components/MidText.jsx
@@ -4,7 +4,7 @@ const MidText = () => {
     return (
         <div className='text-white'>
             <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
-             <p> </p>   
+             <p> Welcome to React Aviation </p>   
             </div>
         </div>
     )
