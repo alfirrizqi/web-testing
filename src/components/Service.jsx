9faa1e99a004d7a4b718e8bdf4a9c61a6a5b6f89
alfirrizqi <ivan_alfirrizqi@outlook.com>
2023-04-09 14:47:05 +0700

    test01

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -9,8 +9,8 @@ const Service = () => {
             <div className='max-w-[1240px] mx-auto grid md:grid-cols-2'>
             <img className='w-[500px] mx-auto my-4' src={Gambar2} alt='/' />
             <div className='flex flex-col justify-center'>
-                <p className='text-[]'>OUR SERVICES</p>
-                <h1>PRIVATE JET CHARTER FOR BUSINESS TRAVEL</h1>
+                <p className='text-[#35e1e3] font-bold'>OUR SERVICES</p>
+                <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'>PRIVATE JET CHARTER FOR BUSINESS TRAVEL</h1>
                 <h2>The benefits of private business aviation are no secret. Successful business people, companies, and start-ups choose to travel by private business jet because it allows them to avoid the crowds, stay productive while in the air, visit multiple locations in a limited time, travel to remote destinations and, when speed is of the essence, a business jet gets you to your destination in the shortest time possible.
                 With a global network of offices, we have access to a vast array of popular private planes that complement your unique travel requirements. We combine this worldwide influence with an unrivalled level of service to create a charter experience that’s bespoke, effortless and luxurious every time.
                 </h2>
